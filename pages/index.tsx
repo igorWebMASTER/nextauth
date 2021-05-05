a1d088ea66cd81c896376fd696cb0df44d5928e4
igorWebMASTER <igor_1917@hotmail.com>
2021-05-04 22:22:25 -0300

    add auth context

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import { useState, FormEvent, useContext} from 'react'
+import styles from '../styles/Home.module.css'
+import { AuthContext } from '../contexts/AuthContext';
+
+export default function Home() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('');
+
+  const { signIn } = useContext(AuthContext)
+
+  function handleSubmit(e : FormEvent){
+    e.preventDefault()
+
+    const data = {
+      email,
+      password,
+    }
+
+    signIn(data)
+  }
+
+  return (
+    
+      <form onSubmit={handleSubmit} className={styles.container}>
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        <button type="submit">Entrar</button>
+      </form>
+  )
+}
