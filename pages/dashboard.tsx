aa30a5ac9231eca0521c417f83921afc605e8af4
igorWebMASTER <igor_1917@hotmail.com>
2021-05-17 23:09:37 -0300

    refactor: create component Can with useCan Hook

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -5,13 +5,10 @@ import { api } from '../services/apiClient'
 import { withSSRAuth } from '../utils/withSSRAuth';
 import { setupAPIClient } from "../services/api";
 import { useCan } from "../hooks/useCan";
+import { Can } from "../components/Can";
 
 export default function Dashboard(){
   const { user } = useContext(AuthContext)
-
-  const userCanSeeMetrics = useCan({
-    roles: [ 'administrator' , 'editor' ]
-  })
 
   useEffect(() => {
     api.get("/me").then(response => console.log(response))
@@ -22,7 +19,11 @@ export default function Dashboard(){
     <>
       <h1>Dashboard: {user?.email}</h1>
 
-      { userCanSeeMetrics && <div>Métricas:</div> }
+      {<Can permissions={['metrics.list']}>
+          <div>
+            Métricas
+          </div>
+        </Can>}
     </>
   )
 }
